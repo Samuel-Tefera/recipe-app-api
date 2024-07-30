236810d7e2bf7c285e77108b28a5f1bd36ae38b7
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-30 10:20:08 +0300

    add test case that check if email is null

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,3 +27,7 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without__email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
