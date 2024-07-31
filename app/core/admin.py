0a58322538027e380c4ebfd79ad5913712889c68
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-31 18:03:55 +0300

    add user admin that manage admin site for user manipulation

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+'''
+Django admin customization.
+'''
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    '''Define the admin pages for users'''
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {
+            'fields' : ('email', 'password')
+        }),
+        (_('Permission'),{
+            'fields' : ('is_staff', 'is_active', 'is_superuser')
+        }),
+        (_('Important dates'), {
+            'fields' : ('last_login',)
+        })
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (None, {
+        'classes' : ('wide',),
+        'fields' : ('name',
+                    'email',
+                    'password1', 'password2',
+                    'is_staff', 'is_active', 'is_superuser'),
+    }),
+
+admin.site.register(models.User, UserAdmin)
