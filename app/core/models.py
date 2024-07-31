7d666e442d3cd87af121fe8670fb3cf4af827ed1
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-31 17:58:10 +0300

    make create super user method function in user manager

--- app/core/models.py
+++ app/core/models.py
@@ -21,8 +21,17 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
 
-class User(AbstractBaseUser):
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     '''User in the system'''
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
