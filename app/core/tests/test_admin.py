9b515d75c0e0699260ecc62a7b107c3156f0c29e
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-31 18:02:10 +0300

    add test cases which checks features that admin site must have

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+'''
+Test from django admin modifications.
+'''
+
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+    '''Tests for django admin.'''
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='adminpass'
+        )
+        self.client.force_login(self.admin_user)
+
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='userpass',
+            name='Test User'
+        )
+
+    def test_user_list(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
