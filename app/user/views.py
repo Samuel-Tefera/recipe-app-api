a9e9956b189fd5a13521b23ef919d6eeb80a2766
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-02 17:51:05 +0300

    create views for creating new user and new token

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+'''
+Views for user API
+'''
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializers,
+    AuthTokenSerializer,)
+
+class CreateUserView(generics.CreateAPIView):
+    '''Create a new user in the system'''
+    serializer_class = UserSerializers
+
+class CreateTokenView(ObtainAuthToken):
+    '''Create a new auth token for user'''
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
