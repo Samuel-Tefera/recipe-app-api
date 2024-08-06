153d9dbc177a77971fb2257b97ab79fca0fa5e92
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-06 18:44:34 +0300

    add url for recipe list api

---
+++ app/recipe/urls.py
@@ -0,0 +1,18 @@
+'''
+URL mapping for recipe app.
+'''
+
+from django.urls  import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+router = DefaultRouter()
+router.register('recipe', views.RecipeViewSets)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
