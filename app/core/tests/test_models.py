e7e85a67b26300e2d9c674f826403b476431ebd4
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-08 13:10:42 +0300

    add test case that check creation of tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,8 +9,13 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
-class ModelTests(TestCase):
 
+def create_user(email='user@example.com', password='testpass'):
+    '''Create and return a new user'''
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         email = 'test@example.com'
         password = 'testpass123'
@@ -60,3 +65,9 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
