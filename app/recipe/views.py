e3f762729167123fdc0edb42bc5cbb39a0947c03
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-10 11:47:31 +0300

    add a view that manage tag listing

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,11 +2,11 @@
 Views for recipe APIs.
 '''
 
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -32,3 +32,15 @@ class RecipeViewSets(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         '''Create a new recipe.'''
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''Manage tags in the datatbase.'''
+    serializer_class = serializers.TagSerializers
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        '''Filter query user for authenticated user.'''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
