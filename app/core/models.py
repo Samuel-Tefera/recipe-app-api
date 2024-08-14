264bab717c53cef94a64cfd01364ddcccf5a1a30
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-14 19:48:36 +0300

    create ingredient model in db

--- app/core/models.py
+++ app/core/models.py
@@ -55,7 +55,8 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-    tags = models.ManyToManyField("Tag")
+    tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -63,6 +64,18 @@ class Recipe(models.Model):
 
 class Tag(models.Model):
     '''Tag object.'''
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    '''Ingredient object.'''
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
