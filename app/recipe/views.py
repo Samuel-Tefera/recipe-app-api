840e7de930a6c93361b8f92cf315db25b9592ef8
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-15 13:15:30 +0300

    create a view for managing ingredient model in db

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -46,4 +46,15 @@ class TagViewSet(mixins.UpdateModelMixin,
 
     def get_queryset(self):
         '''Filter query user for authenticated user.'''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredinetViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''Manage ingredients in the database'''
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
