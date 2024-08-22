6714e6dc223bb7ccf8048d51f4c9340c2a1a1379
Samuel-Tefera <samuelteferab@gmail.com>
2024-08-22 17:45:31 +0300

    add test case that check creating recipe with existing ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -261,7 +261,7 @@ class PrivateRecipeAPITest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
 
-    def test_create_recipe_ingredient(self):
+    def test_create_recipe_with_new_ingredient(self):
         payload = {
             'title' : 'Pie',
             'time_minutes' : 20,
@@ -278,6 +278,30 @@ class PrivateRecipeAPITest(TestCase):
 
         for ings in payload('ingredients'):
             exists = recipe.objects.filter(
+                name=ings['name'],
+                user=self.user
+            ).exists()
+
+            self.assertTrue(exists)
+
+    def test_creating_recipe_with_existing_ingredient(self):
+        ingt = Ingredient.objects.create(user=self.user, name='Pie')
+        payload = {
+            'title' : 'Sweet Pie',
+            'time minutes' : 30,
+            'price' : Decimal('23.89'),
+            'ingredients' : [{'name' : 'Pie'}, {'name' : 'Sugar'}]
+        }
+
+        res = self.client.post(RECIPE_URL, payload, format='json')
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.filter(user=self.user)
+        self.assertEqual(recipe.count(), 1)
+        recipe = recipe[0]
+        self.assertEqual(recipe.ingredients.count(), 2)
+        self.assertIn(ingt, recipe.ingredients.all())
+        for ings in payload['ingredients']:
+            exists = recipe.ingredients.filter(
                 name=ings['name'],
                 user=self.user
             ).exists()
